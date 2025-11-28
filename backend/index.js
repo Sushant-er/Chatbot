94afc235e86879442017b5f8d12c4172e687035a
Sushant-er <iamsushant431@gmail.com>
2025-11-28 16:12:54 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,9 @@ const port=process.env.PORT || 3000
 app.use(express.json());
 app.use(cors());
 
+app.get('/',(req,res)=>{
+    res.send('Welcome to Chatbot API');
+})
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
